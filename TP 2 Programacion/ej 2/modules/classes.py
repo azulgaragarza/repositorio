17c021f8f341a4c3e3472d6916a58ce3e5da3c4b
azulgaragarza <azulgaragar@gmail.com>
2023-06-16 13:43:22 -0300

    actualizo TP 2

--- TP 2 Programacion/ej 2/modules/classes.py
+++ TP 2 Programacion/ej 2/modules/classes.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 class Alimentos():
     def __init__(self):
+        self.aw_alimentos=0
         pass
 
 class Verduras(Alimentos):
@@ -15,13 +16,13 @@ class Frutas(Alimentos):
     def aw_frutas(self):
         raise NotImplementedError("La subclase debe implementar el metodo")
 
-class Kiwi():
+class Kiwi(Frutas):
     def __init__(self):
         self.C=18
     def aw_frutas(self,m):
         self.aw_kiwi=(0.96*((1-((math.e)**(-self.C*m)))/(1+((math.e)**(-self.C*m)))))
     
-class Manzana():
+class Manzana(Frutas):
     def __init__(self):
         self.C=15
     def aw_frutas(self,m):
@@ -39,49 +40,126 @@ class Zanahoria(Verduras):
     def aw_verduras(self,m):
         self.aw_zanahoria=0.96*(1-(math.e**(-self.C*m)))
 
-class aw_promedio():
-    def __init__(self,lista_k,lista_m,lista_z,lista_p):
-        self.aw_kiwis = lista_k
-        self.aw_manzanas = lista_m
-        self.aw_zanahorias = lista_z
-        self.aw_papas = lista_p
+class Cinta_Transportadora():
+    def __init__(self,pesos,alimentos):
+        self.pesos=pesos
+        self.alimentos=alimentos
+        self.lista_objetos=[]
+    def crear_objetos(self):
+        for x in range(len(self.alimentos)):
+            if self.alimentos[x]=="kiwi":
+                objeto=Kiwi()
+                objeto.aw_frutas(self.pesos[x]) #le asigna un peso a al objeto
+                self.lista_objetos.append(objeto)
+            elif self.alimentos[x]=="manzana":
+                objeto=Manzana()
+                objeto.aw_frutas(self.pesos[x])
+                self.lista_objetos.append(objeto)
+            elif self.alimentos[x]=="zanahoria":
+                objeto=Zanahoria()
+                objeto.aw_verduras(self.pesos[x])
+                self.lista_objetos.append(objeto)
+            elif self.alimentos[x]=="papa":
+                objeto=Papa()
+                objeto.aw_verduras(self.pesos[x])
+                self.lista_objetos.append(objeto)
+            else:
+                next
 
+class Cajon(Cinta_Transportadora):
     def aw_prom_kiwis(self):
         suma=0
-        for k in range(len(self.aw_kiwis)):
-            suma=suma+self.aw_kiwis[k]
-        promedio_k=round(suma/len(self.aw_kiwis),2)
-        self.prom_k=promedio_k
-
+        contador=0
+        for k in range(len(self.lista_objetos)):
+            if isinstance(self.lista_objetos[k],Kiwi)==True:
+                suma=suma+self.lista_objetos[k].aw_kiwi
+                contador=contador+1
+        if contador!=0:
+            self.prom_k=round(suma/contador,2)
+        else:
+            self.prom_k=0
+        
     def aw_prom_manzanas(self):
         suma=0
-        for m in range(len(self.aw_manzanas)):
-            suma=suma+self.aw_manzanas[m]
-        promedio_m=round(suma/len(self.aw_manzanas),2)
-        self.prom_m=promedio_m
+        contador=0
+        for m in range(len(self.lista_objetos)):
+            if isinstance(self.lista_objetos[m],Manzana)==True:
+                suma=suma+self.lista_objetos[m].aw_manzana
+                contador=contador+1
+        if contador!=0:
+            self.prom_m=round(suma/contador,2)
+        else:
+            self.prom_m=0
         
     def aw_prom_zanahorias(self):
         suma=0
-        for z in range(len(self.aw_zanahorias)):
-            suma=suma+self.aw_zanahorias[z]
-        promedio_z=round(suma/len(self.aw_zanahorias),2)
-        self.prom_z=promedio_z
-
+        contador=0
+        for z in range(len(self.lista_objetos)):
+            if isinstance(self.lista_objetos[z],Zanahoria)==True:
+                suma=suma+self.lista_objetos[z].aw_zanahoria
+                contador=contador+1
+        if contador!=0:
+            self.prom_z=round(suma/contador,2)
+        else:
+            self.prom_z=0
+        
     def aw_prom_papa(self):
         suma=0
-        for p in range(len(self.aw_papas)):
-            suma=suma+self.aw_papas[p]
-        promedio_p=round(suma/len(self.aw_papas),2)
-        self.prom_p=promedio_p
-
-    def aw_prom_verduras(self):
-        self.prom_verduras= round(((self.prom_p + self.prom_z)/2),2)
+        contador=0
+        for p in range(len(self.lista_objetos)):
+            if isinstance(self.lista_objetos[p],Papa)==True:
+                suma=suma+self.lista_objetos[p].aw_papa
+                contador=contador+1
+        if contador!=0:
+            self.prom_p=round(suma/contador,2)
+        else:
+            self.prom_p=0
     
-    def aw_prom_frutas(self):
-        self.prom_frutas= round(((self.prom_k + self.prom_m)/2),2)
+    def aw_prom_verduras(self):
+        suma=0
+        contador=0
+        for v in range(len(self.lista_objetos)):
+            if isinstance(self.lista_objetos[v],Verduras):
+                if isinstance(self.lista_objetos[v],Papa):
+                    suma=suma+self.lista_objetos[v].aw_papa
+                    contador=contador+1
+                else:
+                    suma=suma+self.lista_objetos[v].aw_zanahoria
+                    contador=contador+1
+        if contador!=0:
+            self.prom_verduras=round(suma/contador,2)
+        else:
+            self.prom_verduras=0
 
-    def prom_total(self):
-        self.promedio_total= round(((self.prom_verduras + self.prom_frutas)/2),2)
+    def aw_prom_frutas(self):
+        suma=0
+        contador=0
+        for f in range(len(self.lista_objetos)):
+            if isinstance(self.lista_objetos[f],Frutas):
+                if isinstance(self.lista_objetos[f],Kiwi):
+                    suma=suma+self.lista_objetos[f].aw_kiwi
+                    contador=contador+1
+                else:
+                    suma=suma+self.lista_objetos[f].aw_manzana
+                    contador=contador+1
+        if contador!=0:
+            self.prom_frutas=round(suma/contador,2)
+        else:
+            self.prom_frutas=0
+        
+    def prom_total(self,cant_alimentos):
+        suma=0
+        for t in self.lista_objetos:
+            if isinstance(t,Kiwi):
+                suma=suma+t.aw_kiwi
+            elif isinstance(t,Manzana):
+                suma=suma+t.aw_manzana
+            elif isinstance(t,Zanahoria):
+                suma=suma+t.aw_zanahoria
+            else:
+                suma=suma+t.aw_papa
+        self.promedio_total= round((suma/cant_alimentos),2)
+
 
 class DetectorAlimento:
     """clase que representa un conjunto de sensores de la cinta transportadora
--- TP 2 Programacion/ej 2/server.py
+++ TP 2 Programacion/ej 2/server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from modules.classes import *
-from modules.functions import *
 import random
 lista_objetos = []
 aw_promedio_kiwi = 0
@@ -15,46 +14,46 @@ app = Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 
 def home():
-    mensaje="Se recomienda inspeccion del cajon"
-
+    mensaje=""
     if request.method == 'POST':
         cant_alimentos=request.form["cant_alimentos"]
         random.seed(1)
         sensor = DetectorAlimento()
         lista_pesos = []
         lista_alimentos = []
-        lista_objetos=[]
-        for i in range(cant_alimentos):
+        for i in range(int(cant_alimentos)):
             lista_pesos.append(sensor.detectar_alimento()["peso"])
             lista_alimentos.append(sensor.detectar_alimento()["alimento"])
-        
-        crear_objetos(lista_pesos,lista_alimentos,lista_objetos)
 
-        promedio = aw_promedio(obj_kiwi,obj_manzana,obj_zanahoria,obj_papa)
-        promedio.aw_prom_kiwis()
-        promedio.aw_prom_manzanas()
-        promedio.aw_prom_papa()
-        promedio.aw_prom_zanahorias()
-        promedio.aw_prom_frutas()
-        promedio.aw_prom_verduras()
-        promedio.prom_total()
+        cajon = Cajon(lista_pesos,lista_alimentos)
+        cajon.crear_objetos()
+        cajon.aw_prom_kiwis()
+        cajon.aw_prom_manzanas()
+        cajon.aw_prom_papa()
+        cajon.aw_prom_zanahorias()
+        cajon.aw_prom_frutas()
+        cajon.aw_prom_verduras()
+        cajon.prom_total(int(cant_alimentos))
 
         global aw_promedio_kiwi 
-        aw_promedio_kiwi = promedio.prom_k
+        aw_promedio_kiwi = cajon.prom_k
         global aw_promedio_manzana 
-        aw_promedio_manzana = promedio.prom_m
+        aw_promedio_manzana = cajon.prom_m
         global aw_promedio_zanahoria
-        aw_promedio_zanahoria = promedio.prom_z
+        aw_promedio_zanahoria = cajon.prom_z
         global aw_promedio_papa
-        aw_promedio_papa = promedio.prom_p
+        aw_promedio_papa = cajon.prom_p
         global aw_promedio_frutas 
-        aw_promedio_frutas = promedio.prom_frutas
+        aw_promedio_frutas = cajon.prom_frutas
         global aw_promedio_verduras
-        aw_promedio_verduras = promedio.prom_verduras
+        aw_promedio_verduras = cajon.prom_verduras
         global aw_promedio_total 
-        aw_promedio_total = promedio.promedio_total
+        aw_promedio_total = cajon.promedio_total
 
-    return render_template('Home.html',mensaje=mensaje,prom_kiwi=aw_promedio_kiwi, prom_manzana=aw_promedio_manzana, prom_zana=aw_promedio_zanahoria,prom_papa=aw_promedio_papa,prom_frutas=aw_promedio_frutas, prom_verdu=aw_promedio_verduras,prom_total=aw_promedio_total)
+        if aw_promedio_kiwi>=0.95 or aw_promedio_manzana>=0.95 or aw_promedio_zanahoria>=0.95 or aw_promedio_papa>=0.95:
+            mensaje="Se recomienda inspeccion del cajon" 
+
+    return render_template('Home.html', mensaje=mensaje, prom_kiwi=aw_promedio_kiwi, prom_manzana=aw_promedio_manzana, prom_zana=aw_promedio_zanahoria,prom_papa=aw_promedio_papa,prom_frutas=aw_promedio_frutas, prom_verdu=aw_promedio_verduras,prom_total=aw_promedio_total)
 
 if __name__ == '__main__':
    app.run(debug = True)
